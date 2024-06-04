fca85e34718b7d8b664b13ca3c88276b44ef26c0
Jurica Migac <jurica1.migac@gmail.com>
2024-06-04 18:35:56 +0200

    Modularizing for Test deployment

--- api/index.py
+++ api/index.py
@@ -1,9 +1,8 @@
 from flask import Response, Flask
-from glucose.glucose_api import glucose_api
+from glucose.glucose_apis import glucose_api
 
 app = Flask(__name__)
 app.register_blueprint(glucose_api)
-
 
 @app.route("/")
 def index():
