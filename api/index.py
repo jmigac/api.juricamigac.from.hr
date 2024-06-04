ba3ae808bf342123748773043616697de89686d2
Jurica Migac <jurica1.migac@gmail.com>
2024-06-04 19:00:01 +0200

    Adding Templates and Styles

--- api/index.py
+++ api/index.py
@@ -1,13 +1,11 @@
-from flask import Response, Flask
+from flask import Flask, render_template
 from api.glucose.glucose_apis import glucose_api
 
 app = Flask(__name__)
 app.register_blueprint(glucose_api)
+app.static_folder = "static"
 
 
 @app.route("/")
 def index():
-    return Response(response="Home Page",
-                    headers={'Access-Control-Allow-Origin': '*'},
-                    mimetype="application/json")
-
+    return render_template('index.html')
