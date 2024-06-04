01c5b722bf82f09faee55d9fd9819cc5b7787757
Jurica Migač <jurica1.migac@gmail.com>
2024-06-04 18:25:58 +0200

    Enabling Glucose Endpoints and Application Modularizing (#2)
    * Glucose API migration
    
    * Enabling API Endpoints, Blueprints, Modularizing

---
+++ api/glucose/glucose_api.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+from flask import Blueprint, Response
+from supabase import create_client
+
+glucose_api = Blueprint("glucose_api", __name__, template_folder="glucose")
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+database = create_client(url, key)
+
+@glucose_api.route("/v1/glucose")
+def get_latest_glucose_reading():
+    data, count = database.table('glucose').select('*').order('date', desc=True).limit(1).single().execute()
+    data_json = json.dumps(data[1], indent=4)
+    return Response(data_json,
+                    headers={'Access-Control-Allow-Origin': '*'},
+                    mimetype="application/json")
+
+
+@glucose_api.route("/v1/glucose/all")
+def get_all_glucose_reading():
+    data, count = database.table('glucose').select('*').order('date', desc=True).limit(10).execute()
+    data_json = json.dumps(data[1], indent=4)
+    return Response(data_json,
+                    headers={'Access-Control-Allow-Origin': '*'},
+                    mimetype="application/json")
+
+
+@glucose_api.route("/glucose/<glucose_value>")
+def insert_glucose_value(glucose_value):
+    database.table('glucose').insert({"value": glucose_value}).execute()
+    return Response("Glucose level successfully inserted",
+                    headers={'Access-Control-Allow-Origin': '*'},
+                    mimetype="application/json")
--- api/index.py
+++ api/index.py
@@ -1,35 +1,12 @@
-import json
-import os
 from flask import Response, Flask
-from supabase import create_client, Client
+from glucose.glucose_api import glucose_api
 
 app = Flask(__name__)
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
-Client = create_client(url, key)
+app.register_blueprint(glucose_api)
 
 
-@app.route("/v1/glucose")
-def get_latest():
-    data, count = Client.table('glucose').select('*').order('date', desc=True).limit(1).single().execute()
-    data_json = json.dumps(data[1], indent=4)
-    return Response(data_json,
-                    headers={'Access-Control-Allow-Origin': '*'},
-                    mimetype="application/json")
-
-
-@app.route("/v1/glucose/all")
-def get_latest():
-    data, count = Client.table('glucose').select('*').order('date', desc=True).limit(10).execute()
-    data_json = json.dumps(data[1], indent=4)
-    return Response(data_json,
-                    headers={'Access-Control-Allow-Origin': '*'},
-                    mimetype="application/json")
-
-
-@app.route("/glucose/<glucose_value>")
-def insert_glucose_value(glucose_value):
-    Client.table('glucose').insert({"value": glucose_value}).execute()
-    return Response("Glucose level successfully inserted",
+@app.route("/")
+def index():
+    return Response(response="Home Page",
                     headers={'Access-Control-Allow-Origin': '*'},
                     mimetype="application/json")
