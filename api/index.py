d559eb7f9cb978f5f03d900e13452eef30a0476f
Jurica Migac <jurica1.migac@gmail.com>
2024-06-04 18:40:15 +0200

    Testing with application run

--- api/index.py
+++ api/index.py
@@ -4,8 +4,13 @@ from glucose.glucose_apis import glucose_api
 app = Flask(__name__)
 app.register_blueprint(glucose_api)
 
+
 @app.route("/")
 def index():
     return Response(response="Home Page",
                     headers={'Access-Control-Allow-Origin': '*'},
                     mimetype="application/json")
+
+
+if __name__ == "__main__":
+    app.run()
